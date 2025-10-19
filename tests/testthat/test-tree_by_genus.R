ebf9412e16b208c98221f5ed6bd0d0edb31d4111
David <davidgd2015@gmail.com>
2025-10-19 17:21:52 -0300

    add tests

--- tests/testthat/test-tree_by_genus.R
+++ tests/testthat/test-tree_by_genus.R
@@ -9,3 +9,15 @@ test_that("tree_by_genus is precise with relationships", {
   rel <- matrix(c(10, 11, 11, 10, 12, 13, 13, 12, 14, 14, 14, 14, 14, 11, 1, 2, 12, 13, 3, 4, 14, 5, 6, 7, 8, 9), ncol=2)
   expect_equal(test_tree$edge, rel)
 })
+
+test_that("tree_by_genus is standarizing names", {
+  test_tree <- tree_by_genus(c("Bazzania", "BAZZANIA", "calypogeia", "TRIChocolea", "lEIOMITRA", "Mnioloma", "Paracromastigum", "Pseudocephalozia", "Telaranea", "Zoopsidella"))
+  rel_tip_label <- c("Calypogeia", "Mnioloma", "Trichocolea", "Leiomitra", "Bazzania", "Paracromastigum", "Pseudocephalozia", "Telaranea", "Zoopsidella")
+  expect_equal(test_tree$tip.label, rel_tip_label)
+})
+
+test_that("tree_by_genus is idenifying the mismatches", {
+  w <- "The following genus were not found in the input tree and will be ignored: Calypogia, Leiomitreaa"
+  expect_warning(tree_by_genus(c("Bazzania", "Bazzania", "Calypogia", "Trichocolea", "Leiomitreaa", "Mnioloma", "Paracromastigum", "Pseudocephalozia", "Telaranea", "Zoopsidella")),
+                 w)
+})
--- tests/testthat/test-tree_by_species.R
+++ tests/testthat/test-tree_by_species.R
@@ -1,11 +1,23 @@
 library(testthat)
 
 test_that("tree_by_species prints a 'phylo' object", {
-  expect_s3_class(tree_by_species(c("Herbertus_sendtneri", "Micropterygium_carinatum", "Lepidozia_pinnaticruris", "Bazzania_pallidevirens", "Bazzania_jamaicensis", "Plagiochila_simplex", "Plagiochila_revolvens")), "phylo")
+  expect_s3_class(tree_by_species(c("Herbertus sendtneri", "Micropterygium carinatum", "Lepidozia pinnaticruris", "Bazzania pallidevirens", "Bazzania jamaicensis", "Plagiochila simplex", "Plagiochila revolvens")), "phylo")
 })
 
 test_that("tree_by_species is precise with relationships", {
-  test_tree <- tree_by_species(c("Herbertus_sendtneri", "Micropterygium_carinatum", "Lepidozia_pinnaticruris", "Bazzania_pallidevirens", "Bazzania_jamaicensis", "Plagiochila_simplex", "Plagiochila_revolvens"))
+  test_tree <- tree_by_species(c("Herbertus sendtneri", "Micropterygium carinatum", "Lepidozia pinnaticruris", "Bazzania pallidevirens", "Bazzania jamaicensis", "Plagiochila simplex", "Plagiochila revolvens"))
   rel <- matrix(c(8, 9, 9, 8, 10, 11, 12, 12, 11, 11, 10, 9, 1, 2, 10, 11, 12, 3, 4, 5, 6, 7), ncol=2)
   expect_equal(test_tree$edge, rel)
+})
+
+test_that("tree_by_species is standarizing names", {
+  test_tree <- tree_by_species(c("Herbertus_sendtneri", "Micropterygium Carinatum", "LEPIDOZIA PINNATICRURIS", "Bazzania_PALLIDEVIRENS", "BAZZANIA jamaicensis", "Plagiochila simplex", "Plagiochila revolvens"))
+  rel_tip_label <- c("Plagiochila revolvens", "Plagiochila simplex", "Bazzania jamaicensis", "Bazzania pallidevirens", "Lepidozia pinnaticruris", "Micropterygium carinatum", "Herbertus sendtneri")
+  expect_equal(test_tree$tip.label, rel_tip_label)
+})
+
+test_that("tree_by_species is idenifying the mismatches", {
+  w <- "The following species were not found in the input tree and will be ignored: Herbertus sendtnaeri, Micropterygium caritum"
+  expect_warning(tree_by_species(c("Herbertus sendtnaeri", "Micropterygium caritum", "Lepidozia pinnaticruris", "Bazzania pallidevirens", "Bazzania jamaicensis", "Plagiochila simplex", "Plagiochila revolvens")),
+                 w)
 })
